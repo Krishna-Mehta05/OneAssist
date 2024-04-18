9627d6c44650ab792ebd74bb8ab50d6f9f1cedc5
Krishna-Mehta05 <krishna.mehta@oneadvanced.com>
2024-04-18 16:51:31 +0530

    fix: scenario

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -76,31 +76,31 @@ module.exports.addMessage = async (req, res, next) => {
           Happy to help. If you have a few minutes I would love you to stay on the chat and complete a short feedback survey. Thank you have a good day!
             `
         })
-      } else if (message.indexOf('legal software solution  ') !== -1) {
+      } else if (message.indexOf('legal software solution ') !== -1) {
         return res.json({
           msg: `
           Thank you for sharing your needs, we definitely have a solution that solves your challenges. Could you please provide us with your contact information like Full Name, Job Title, Email, Company Name, Marketing Opt In so that we can personalise your experience today?
             `
         })
-      } else if (message.indexOf('Full name  ') !== -1) {
+      } else if (message.indexOf('Sandra ') !== -1) {
         return res.json({
           msg: `
           Thank you Sandra, we will now be able to provide you with better assistance and only provide you with useful resources that support you during the research stage. It sounds like you're looking for a solution that not only simplifies your daily operations but also aligns with the size and scope of your firm. Our new software portfolio, OneAdvanced Legal is an excellent choice for small to medium-sized law firms looking to enhance efficiency and productivity. It’s designed to address challenges just like yours, offering seamless case management, document handling, and time tracking in one integrated platform.
             `
         })
-      } else if (message.indexOf('sounds promising  ') !== -1) {
+      } else if (message.indexOf('sounds promising ') !== -1) {
         return res.json({
           msg: `
           Absolutely! OneAdvanced Legal empowers firms like yours by automating routine tasks, which frees up your team to focus on more strategic work. For instance, its intuitive case management feature allows you to access all case-related information in one place, improving collaboration and decision-making. The platform also includes advanced document management capabilities, making it easier to store, search, and share documents securely. Plus, with its time tracking and billing features, you can streamline your invoicing process, ensuring accuracy and saving valuable time. Here is a customer testimonial with similar challenges as your law firm www.example.com`
         })
-      } else if (message.indexOf('game-changer ') !== -1) {
+      } else if (message.indexOf('game-changer') !== -1) {
         return res.json({
           msg: `
           I'm glad to hear it resonates with your needs! On our detailed product page you will find comprehensive information on features, benefits, and how it addresses common challenges faced by firms like yours. The OneAdvanced Legal portfolio also comes with a fantastic eLearning course so that your team can get familiar with the portfolio in their own time, but the OneAdvanced team are also always here to support https://www.oneadvanced.com/products/oneadvanced-legal/  (Open in new tab so prospect can stay on the chat)
           Additionally, now that you have provided your contact details, we will send you related resources tailored to your interests via email.
 `
         })
-      } else if (message.indexOf('looked at the product page ') !== -1) {
+      } else if (message.indexOf('will dive in ') !== -1) {
         return res.json({
           msg: `
           Of course! We would love to speak with you. Would you like me to forward your details and enquiry to one of our expert legal software consultants?
